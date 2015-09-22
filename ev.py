6dd2b5c3172bd76f0a1aac658f0bc1cc85566c14
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-09-22 21:38:45 +0200

    Added main method

--- ev.py
+++ ev.py
@@ -6,13 +6,14 @@ import sys
 
 
 def compile(file_name, problem_name, file_type):
-    command = ""
     if file_type == "c":
         command = ' '.join(['gcc', file_name, '-o', problem_name])
     elif file_type == "cpp":
         command = ' '.join(['g++', file_name, '-o', problem_name])
     elif file_type == "java":
         command = ' '.join(['javac', file_name])
+    else:
+        command = ""
 
     try:
         subprocess.check_output(command, shell=True)
@@ -27,24 +28,28 @@ def run(file_name, problem_name, file_type):
         subprocess.call(["java", problem_name])
     elif file_type == "py":
         subprocess.call(["python", file_name])
-    elif file_type == "rb":
-        subprocess.call(["ruby", file_name])
     else:
-        pass
+        subprocess.call(["ruby", file_name])
 
-argument = sys.argv[1]
-if not os.path.isfile(argument):
-    print "No such file!"
-    sys.exit(1)
 
-file_name_struct = argument.split(".")
-problem_name = file_name_struct[0]
-file_type = file_name_struct[-1]
+def main():
+    argument = sys.argv[1]
+    if not os.path.isfile(argument):
+        print "No such file!"
+        sys.exit(1)
 
-supported_languages = ["c", "cpp", "py", "rb", "java"]
-if file_type not in supported_languages:
-    print "Language not supported"
-    sys.exit(1)
+    file_name_struct = argument.split(".")
+    problem_name = file_name_struct[0]
+    file_type = file_name_struct[-1]
 
-compile(argument, problem_name, file_type)
-run(argument, problem_name, file_type)
+    supported_languages = ["c", "cpp", "py", "rb", "java"]
+    if file_type not in supported_languages:
+        print "Language not supported"
+        sys.exit(1)
+
+    compile(argument, problem_name, file_type)
+    run(argument, problem_name, file_type)
+
+
+if __name__ == "__main__":
+    main()
