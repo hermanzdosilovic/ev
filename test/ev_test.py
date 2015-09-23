3ed9bca62afc5e5db87fe5dbb6817364649801c5
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-09-23 22:29:46 +0200

    We have first testcase

---
+++ test/ev_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import unittest
+import pdb
+
+from sys import path
+path.append("..")
+import ev
+
+
+class SimpleEvTest(unittest.TestCase):
+    def setUp(self):
+        self.c_file_name = "hello.c"
+        self.c_problem_name = "hello"
+        self.c_file_type = "c"
+
+class EvTest(SimpleEvTest):
+    def test_compile_with_gcc(self):
+        with self.assertRaises(Exception):
+            ev.compile(self.c_file_name, self.c_problem_name, self.c_file_type)
+
+if __name__ == '__main__':
+    unittest.main()
