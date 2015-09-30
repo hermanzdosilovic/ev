f288ce9c8f6edc2dc5088e1b3ffdef02591aaa03
Ivan Krpelnik <ikrpelnik@Ivans-MacBook-Pro.local>
2015-09-30 22:06:48 +0200

    Refactored to Tester class, added input/output

--- basic_timer.py
+++ basic_timer.py
@@ -8,37 +8,53 @@ import atexit
 import sys
 
 ON_WINDOWS = (sys.platform == 'win32')
-TIMEOUT = 1
-TIMEOUT_OFFSET = 0.5
-DEFAULT_INT = -1
 WA = 0
 AC = 1
 TLE = 2
 
-def run_program(child_pid, time_taken, program_name):
-    prog = subprocess.Popen(['./' + program_name])
-    child_pid.value = prog.pid
-    start_time = timeit.default_timer()
-    prog.wait()
-    time_taken.value = timeit.default_timer() - start_time
-    return
+class Result:
+    WA = 0
+    AC = 1
+    TLE = 2
+    def __init__(self, status, time):
+        self.status = status
+        self.time = time
 
-def start_test(program_name):
-    child_pid = multiprocessing.Value('i', DEFAULT_INT)
-    time_taken = multiprocessing.Value('d', DEFAULT_INT)
-    run_process = multiprocessing.Process(target=run_program, args=[child_pid, time_taken, program_name])
-    run_process.start()
-    run_process.join(TIMEOUT + TIMEOUT_OFFSET)
-    is_tle = run_process.is_alive()
-    if run_process.is_alive() == True: 
-        run_process.terminate()
-        if child_pid.value != -1:
-            os.kill(child_pid.value, signal.CTRL_BREAK_EVENT if ON_WINDOWS else signal.SIGTERM)
+class Tester:
+    DEFAULT_INT = -1
     
-    if time_taken.value != DEFAULT_INT:
-        is_tle = time_taken.value > TIMEOUT
-    ac = AC
-    return TLE if is_tle else ac  
+    def __init__(self, timeout, timeout_offset):
+        self.timeout = timeout
+        self.timeout_offset = timeout_offset
 
-result = start_test('a.out')
-print result
+    def run_program(self, child_pid, time_taken, command, in_file, out_file):
+        input_file = open(in_file, 'r')
+        output_file = open(out_file, 'w')
+        start_time = timeit.default_timer()
+        prog = subprocess.Popen(args=[command], stdin=input_file, stdout=output_file)
+        child_pid.value = prog.pid
+        prog.communicate();
+        time_taken.value = timeit.default_timer() - start_time
+        return
+
+    def start_test(self, command, in_file, out_file, user_out):
+        child_pid = multiprocessing.Value('i', self.DEFAULT_INT)
+        time_taken = multiprocessing.Value('d', self.DEFAULT_INT)
+        run_process = multiprocessing.Process(target=self.run_program, args=[child_pid, time_taken, command, in_file, user_out])
+        run_process.start()
+        run_process.join(self.timeout + self.timeout_offset)
+        is_tle = run_process.is_alive()
+        if run_process.is_alive() == True: 
+            run_process.terminate()
+            if child_pid.value != -1:
+                os.kill(child_pid.value, signal.CTRL_BREAK_EVENT if ON_WINDOWS else signal.SIGTERM)
+        
+        if time_taken.value != self.DEFAULT_INT and time_taken.value > self.timeout or time_taken.value == self.DEFAULT_INT:
+            return Result(Result.TLE, time_taken.value)
+        #usporedi out_file i user_out
+        ac = AC
+        return Result(ac, time_taken.value)
+
+tester = Tester(1, 0.5)
+result = tester.start_test('./a.out', './in.txt', './out.txt', './user.txt')
+print str(result.status) + " " + str(result.time)
